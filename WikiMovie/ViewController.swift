e9ccddc7be02f25676d6d56094f5f2518241266c
nahuelterrazas <nterrazasalcocer@frba.utn.edu.ar>
2022-07-19 03:55:46 -0300

    Initial Commit

---
+++ WikiMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WikiMovie
+//
+//  Created by Nahuel Terrazas on 19/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
